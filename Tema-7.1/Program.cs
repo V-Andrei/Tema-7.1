423314c6b9ffbbdabbb446730acf09e00d8d9992
Andrei Vrancianu <vrancianuandrei@gmail.com>
2019-11-21 16:39:15 +0200

    FirstUpload

---
+++ Tema-7.1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security;
+
+namespace Tema_7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1_File();
+            //Ex2_WebFile();
+            //Ex3_EnterNumbers();
+
+            Console.ReadKey();
+        }
+
+        private static void Ex3_EnterNumbers()
+        {
+            int start = 0;
+            int end = 100;
+            const int count = 10;
+
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            try
+            {
+                if (numbers.Any(x => x < start) || numbers.Any(x => x > end) || !IsIncreasing(numbers))
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine("1 < " + string.Join(" < ", numbers) + " < 100");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+
+
+            static bool IsIncreasing(List<int> numbers)
+            {
+                for (int i = 1; i < numbers.Count; i++)
+                {
+
+                    if (numbers[i - 1].CompareTo(numbers[i]) >= 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        private static void Ex2_WebFile()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    Console.WriteLine("\nStart downloading...");
+                    webClient.DownloadFile("https://image.shutterstock.com/image-vector/cute-happy-ninja-smiling-his-260nw-359792546.jpg", "../../ninja.jpg");
+                    Console.WriteLine("\n-> Download successfully!");
+                }
+
+                catch (ArgumentException)
+                {
+                    Console.Error.WriteLine("\n-> Error: You have entered an empty URL!");
+                }
+                catch (WebException)
+                {
+                    Console.Error.WriteLine("\n-> Error: You have entered an invalid URL!");
+                }
+                catch (NotSupportedException)
+                {
+                    Console.Error.WriteLine("\n-> Error: This method does not support simultaneous downloads!");
+                }
+                finally
+                {
+                    Console.WriteLine("\nDone!\n");
+                }
+            }
+        }
+
+        private static void Ex1_File()
+        {
+            Console.Write("Enter the full path of the file you want to read: ");
+            string filePath = Console.ReadLine();
+
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine("The content of the file is: ");
+                Console.WriteLine(fileContent);
+            }
+
+            catch (ArgumentNullException)
+            {
+                Console.Error.WriteLine("Path is null.");
+            }
+
+            catch (ArgumentException)
+            {
+                Console.Error.WriteLine("Path is a zero-length string, contains only white space, or contains one or more invalid characters.");
+            }
+
+            catch (PathTooLongException)
+            {
+                Console.Error.WriteLine("The specified path, file name, or both exceed the system-defined maximum length.");
+            }
+
+            catch (DirectoryNotFoundException)
+            {
+                Console.Error.WriteLine("The specified path is invalid.");
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("The file specified in path was not found.");
+            }
+
+            catch (IOException)
+            {
+                Console.Error.WriteLine("An I/O error occurred while opening the file.");
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+                Console.Error.WriteLine("The caller does not have the required permission.");
+            }
+
+            catch (NotSupportedException)
+            {
+                Console.Error.WriteLine("Path is in an invalid format.");
+            }
+
+            catch (SecurityException)
+            {
+                Console.Error.WriteLine("The caller does not have the required permission.");
+            }
+        }
+    }
+}
